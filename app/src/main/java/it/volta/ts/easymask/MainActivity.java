f7af77232f4b1377f51aa1465dfdf984e643b06c
des98 <desertcod98@gmail.com>
2022-05-23 17:48:55 +0200

    added qr scanning (alpha)

--- app/src/main/java/it/volta/ts/easymask/MainActivity.java
+++ app/src/main/java/it/volta/ts/easymask/MainActivity.java
@@ -2,7 +2,10 @@ package it.volta.ts.easymask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import it.volta.ts.easymask.activities.QrCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, QrCaptureActivity.class);
+        startActivity(intent); // :D crasha lol
+
     }
 }
