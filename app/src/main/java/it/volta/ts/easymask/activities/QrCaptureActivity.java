478057a57ffd40787fb1d2b4d3567c248cf0306a
des98 <desertcod98@gmail.com>
2022-05-23 18:02:02 +0200

    created new activity_qr_capture xml

--- app/src/main/java/it/volta/ts/easymask/activities/QrCaptureActivity.java
+++ app/src/main/java/it/volta/ts/easymask/activities/QrCaptureActivity.java
@@ -18,32 +18,23 @@ import it.volta.ts.easymask.R;
 // implements onClickListener for the onclick behaviour of button
 public class QrCaptureActivity extends AppCompatActivity implements View.OnClickListener {
     Button scanBtn;
-    TextView messageText, messageFormat;
+    TextView messageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // referencing and initializing
-        // the button and textviews
+        setContentView(R.layout.activity_qr_capture);
         scanBtn = findViewById(R.id.scanBtn);
         messageText = findViewById(R.id.textContent);
-        messageFormat = findViewById(R.id.textFormat);
-
-        // adding listener to the button
         scanBtn.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        // we need to create the object
-        // of IntentIntegrator class
-        // which is the class of QR library
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
         intentIntegrator.setCaptureActivity(AnyOrientationCaptureActivity.class);
-        intentIntegrator.setPrompt("Scan a barcode or QR Code");
+        intentIntegrator.setPrompt("Scan a barcode or a QR Code");
         intentIntegrator.setOrientationLocked(false);
         intentIntegrator.initiateScan();
     }
@@ -52,16 +43,11 @@ public class QrCaptureActivity extends AppCompatActivity implements View.OnClick
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        // if the intentResult is null then
-        // toast a message as "cancelled"
         if (intentResult != null) {
             if (intentResult.getContents() == null) {
-                Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(), "Scan cancelled", Toast.LENGTH_SHORT).show();
             } else {
-                // if the intentResult is not null we'll set
-                // the content and format of scan message
                 messageText.setText(intentResult.getContents());
-                messageFormat.setText(intentResult.getFormatName());
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
